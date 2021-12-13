32e291e62602a8946f0b9d265367c76b61f0ccc3
asrahim88 <tasmimhasan1997@gmail.com>
2021-12-13 20:03:44 +0600

    Initial commit

--- string formatting.py
+++ string formatting.py
@@ -1,2 +1,7 @@
-print('hello world')
-print("ok done")
+num1 = 10
+num2 = 20
+print(f"{num1} + {num2} = {num1+num2}")
+
+input_one = int(input("Enter 1st number: "))
+input_two = int(input("Enter second number: "))
+print(f"{input_one} + {input_two} = {input_one + input_two}")
